78b32f60a4e0f5ff834bb790293aba8b48a5c22f
Garret Poole <garretpoole@users.noreply.github.com>
2022-03-07 17:36:25 -0800

    @AppStorage for user settings

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+    //monitored and works just like @State
+    //@AppStorage works just like UserDefault but more complex when it comes to larger data types
+    @AppStorage("tapCount") private var tapCount = 0
     
     var body: some View {
         Button("Tap Count: \(tapCount)") {
             tapCount += 1
-            //case sensitive key, use it to read back out
-            UserDefaults.standard.set(tapCount, forKey: "Tap")
         }
     }
 }
