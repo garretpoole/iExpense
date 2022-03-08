5663ceb4f0668dd98e82c9a77ddd780b3566aeb7
Garret Poole <garretpoole@users.noreply.github.com>
2022-03-07 16:59:22 -0800

    @State only works with structs not classes

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -6,11 +6,22 @@
 //
 
 import SwiftUI
+//must use class over structs if data is between multiple views
+class User{
+    var firstname = "Bilbo"
+    var lastname = "Baggins"
+}
 
 struct ContentView: View {
+    //@state doesnt see change bc it doesnt watch value inside class (just class itself)
+    @State private var user = User()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("Your name is \(user.firstname) \(user.lastname)")
+            TextField("First Name", text: $user.firstname)
+            TextField("Last Name", text: $user.lastname)
+        }
     }
 }
 
