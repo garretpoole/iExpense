0f513683b346043b46789bde97ee427a8a54e308
Garret Poole <garretpoole@users.noreply.github.com>
2022-03-07 16:51:44 -0800

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Garret Poole on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
