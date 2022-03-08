106bf3064c6bcbddbab2188b73670d455a47507a
Garret Poole <garretpoole@users.noreply.github.com>
2022-03-07 17:22:19 -0800

    deleting with onDelete() and EditButton()

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,29 +7,40 @@
 
 import SwiftUI
 
-struct SecondView: View{
-    @Environment(\.dismiss) var dismiss
-    
-    let name: String
-    var body : some View {
-        Text("Hello , \(name)!")
-        Button("Dismiss") {
-            dismiss()
-        }
-    }
-}
-
 struct ContentView: View {
-    @State private var showingSheet = false
+    @State private var numbers = [Int]()
+    @State private var current = 1
     
     var body: some View {
-        Button("Show Sheet"){
-            showingSheet.toggle()
-        }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "Tide")
+        NavigationView{
+            VStack{
+                //same as below but onDelete() works for only ForEach
+    //            List(numbers, id: \.self){
+    //                Text("Row \($0)")
+    //            }
+                List{
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add Number") {
+                    numbers.append(current)
+                    current += 1
+                }
+            }
+            .navigationTitle("onDelete()")
+            .toolbar {
+                EditButton()
+            }
         }
     }
+    
+    func removeRows(at offsets: IndexSet){
+        numbers.remove(atOffsets: offsets)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
