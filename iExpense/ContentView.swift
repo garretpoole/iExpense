8c7343df38f137fcd7dcf62975b0037bb449efd1
Garret Poole <garretpoole@users.noreply.github.com>
2022-03-07 17:30:36 -0800

    Storing user info with UserDefaults

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,39 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var numbers = [Int]()
-    @State private var current = 1
+    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
     
     var body: some View {
-        NavigationView{
-            VStack{
-                //same as below but onDelete() works for only ForEach
-    //            List(numbers, id: \.self){
-    //                Text("Row \($0)")
-    //            }
-                List{
-                    ForEach(numbers, id: \.self) {
-                        Text("Row \($0)")
-                    }
-                    .onDelete(perform: removeRows)
-                }
-                
-                Button("Add Number") {
-                    numbers.append(current)
-                    current += 1
-                }
-            }
-            .navigationTitle("onDelete()")
-            .toolbar {
-                EditButton()
-            }
+        Button("Tap Count: \(tapCount)") {
+            tapCount += 1
+            //case sensitive key, use it to read back out
+            UserDefaults.standard.set(tapCount, forKey: "Tap")
         }
     }
-    
-    func removeRows(at offsets: IndexSet){
-        numbers.remove(atOffsets: offsets)
-    }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
